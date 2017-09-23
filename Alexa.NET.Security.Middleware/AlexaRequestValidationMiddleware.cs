87f8eabf2686517d48982a613d12387c7a6abe0a
Thomas Pentenrieder <pentenrieder@outlook.com>
2017-09-23 17:34:35 +0200

    add middleware extension

---
+++ Alexa.NET.Security.Middleware/AlexaRequestValidationMiddleware.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Alexa.NET.Security.Middleware
+{
+    /// <summary>
+    /// An ASP.NET Core Middleware for validating Alexa reqeusts
+    /// </summary>
+    public class AlexaRequestValidationMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="next"></param>
+        public AlexaRequestValidationMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        /// <summary>
+        /// Validate if all necessary parts for a valid request are available
+        /// and pass them to the ReqeustVerification tool
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task Invoke(HttpContext context)
+        {
+            // EnableRewind so the body can be read without causing issues to the request pipeline
+            context.Request.EnableRewind();
+            
+            // Verify SignatureCertChainUrl is present
+            context.Request.Headers.TryGetValue("SignatureCertChainUrl", out var signatureChainUrl);
+            if (String.IsNullOrWhiteSpace(signatureChainUrl))
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                return;
+            }
+
+            Uri certUrl;
+            try
+            {
+                certUrl = new Uri(signatureChainUrl);
+            }
+            catch
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                return;
+            }
+
+            // Verify SignatureCertChainUrl is Signature
+            context.Request.Headers.TryGetValue("Signature", out var signature);
+            if (String.IsNullOrWhiteSpace(signature))
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                return;
+            }
+
+            string body = new StreamReader(context.Request.Body).ReadToEnd();
+            context.Request.Body.Position = 0;
+
+            if (String.IsNullOrWhiteSpace(body))
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                return;
+            }
+            var valid = await RequestVerification.Verify(signature, certUrl, body);
+            if (!valid)
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
+
---
+++ Alexa.NET.Security.Middleware/AlexaRequestValidationMiddlewareExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Alexa.NET.Security.Middleware
+{
+    /// <summary>
+    /// Middleware builder
+    /// </summary>
+    public static class AlexaRequestValidationMiddlewareExtension
+    {
+        /// <summary>
+        /// Add AlexaReqeustValidationMiddleware to the request pipeline
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseAlexaRequestValidation(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<AlexaRequestValidationMiddleware>();
+        }
+    }
+}
